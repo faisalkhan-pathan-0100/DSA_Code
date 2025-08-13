891628443027ea7a249843599d8572a4c26bdf90
Faisalkhan Pathan <faisalkhan.pathan.0100@gmail.com>
2025-08-13 10:01:07 +0530

    update the code with problem statement

--- problem27/MInimumSwapRequiredToGetAll1sInBinaryArray.java
+++ problem27/MInimumSwapRequiredToGetAll1sInBinaryArray.java
@@ -1,5 +1,21 @@
 package DSA;
+/*
+ * Given an array of 0's and 1's, we need to write a program to find the minimum number of swaps required to group all 1's present in the array together.
 
+Examples: Input: arr[] = [1, 0, 1, 0, 1]
+Output: 1
+Explanation: Only 1 swap is required to group all 1's together. Swapping index 1 with 4 will 
+                        give arr[] = [1, 1, 1, 0, 0]
+
+Input: arr[] = [1, 1, 0, 1, 0, 1, 1]
+Output: 2
+Explanation: Only 2 swap is required to group all 1's together. Swapping index 0 with 2 and 
+1 with 4 will  give arr[] = [0, 0, 1, 1, 1, 1, 1]
+
+Input: arr[] = [0, 0, 0]
+Output: -1
+Explanation: No 1s are present in the array, so return -1.
+ */
 public class MInimumSwapRequiredToGetAll1sInBinaryArray {
 	static int minSwap(int[] arr) {
 		int ans=0;
